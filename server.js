452f44c414f11fe345ade7ff369bc7b2bb0bca1d
Patrick <patrick@Patricks-iMac.local>
2023-02-07 18:39:17 -0800

    updated functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const express = require('express');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 //defining the port and initializing express
 const PORT = process.env.PORT || 3001;
@@ -14,11 +15,11 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'employees_db'
+    user: process.env.DB_USER,
+    password:  process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   },
-  console.log(`<br><br><br><br><br> Connected to the employees_db database.`)
+  console.log(`Connected to the employees_db database.`)
 );
 
 let start = false;
@@ -27,39 +28,37 @@ const {headerTemp,employeeQues,roleQues,deptQues,updateQues} = require('./src/te
 const [emp_firstQ,emp_lastQ,emp_roleQ,emp_managerQ] = employeeQues;
 const [role_nameQ,role_payQ,role_deptQ] = roleQues;
 const [dept_nameQ] = deptQues;
+const [updt_nameQ,updt_titleQ] = updateQues;
 
-function deptNames(){
+async function handleReturn(data){
+  await data;
+  console.log(data);
+  init();
+}
+
+async function deptNames(){
   db.query('SELECT dept_name FROM department', function (err, results) {
-    console.log(results);
-})
-init();
-};
-function deptReturn(){
+    return(results);
+  })};
+async function deptReturn(){
 //returns departments
 db.query('SELECT dept_id AS DepartmentID, dept_name AS DepartmentName FROM department', function (err, results) {
-  console.log(results);
-})
+  handleReturn(results);
+})};
 
-init();
-};
-
-function roleReturn(){
+async function roleReturn(){
 //returns roles
 db.query('Select role_name AS JobTitle, role_id AS JobID, role_pay AS JobSalary, dept_name AS JobDepartment FROM roles JOIN department WHERE role_id = dept_id', function (err, results) {
-  console.log(results);
-})
-init();
-};
+  handleReturn(results);
+})};
 
-function empReturn(){
+async function empReturn(){
 //returns employees
 db.query('Select emp_id AS EmployeeID,emp_first_name AS FirstName, emp_last_name AS LastName, role_name AS Job, dept_name AS Department, role_pay AS Salary, emp_manager AS Manager FROM employee JOIN roles,department WHERE emp_role = role_id && role_dept = dept_id', function (err, results) {
-  console.log(results);
-})
-init();
-};
+  handleReturn(results);
+})};
 
-function addDept(){
+async function addDept(){
   inquirer
   .prompt([
     {
@@ -70,12 +69,12 @@ function addDept(){
 ])
 .then((response)=>{
   db.query(`INSERT INTO department(dept_name) VALUES ('${response.dept_name}')`, function (err, results) {
-    !err ? console.log(`Success! ${response.dept_name} added!`): console.log(err);
+    !err ? handleReturn(`Success! ${response.dept_name} added!`): handleReturn(err);
 })
-
-init();
 })};
-function addRole(){
+async function addRole(){
+let dept_choices = await deptNames()
+console.log(dept_choices);
   inquirer
   .prompt([
       {
@@ -89,20 +88,17 @@ function addRole(){
           name:'role_pay'
       },
       {
-          type:'list',
+          type:'input',
           message: role_deptQ,
-          choices:[deptNames()],
           name:'role_dept'
       }
 ])
 .then((response)=>{
-  db.query(`INSERT INTO roles (role_name,role_pay,role_dept) VALUES ('${response.role_name}','${response.role_pay}',1)`, function (err, results) {
-    !err ? console.log(`Success! ${response.role_name} added!`): console.log(err);
-  })
-
-  init();
-  })};
-function addEmp(){
+   db.query(`INSERT INTO roles (role_name,role_pay,role_dept) VALUES ('${response.role_name}','${response.role_pay}',${response.role_dept})`, function (err, results) {
+    !err ? handleReturn(`Success! ${response.role_name} added!`): handleReturn(err);
+    })
+})};
+async function addEmp() {
   inquirer
   .prompt([
     {
@@ -127,14 +123,13 @@ function addEmp(){
   }
 ])
 .then((response)=>{
-  db.query(`INSERT INTO employee(emp_first_name,emp_last_name,emp_manager) VALUES ('${response.emp_first}', '${response.emp_last}', '${response.emp_role}','${response.emp_manager}')`, function (err, results) {
-    !err ? console.log(`Success! ${response.emp_first} ${response.emp_last} added!`): console.log(err);
+  db.query(`INSERT INTO employee(emp_first_name,emp_last_name,emp_role,emp_manager) VALUES ('${response.emp_first}', '${response.emp_last}', '${response.emp_role}','${response.emp_manager}')`, function (err, results) {
+    !err ? handleReturn(`Success! ${response.emp_first} ${response.emp_last} added!`): handleReturn(err);
+  })
   })
 
-  init();
-  })};
-
-function addEmp(){
+};
+async function updateEmp(){
   inquirer
   .prompt([
     {
@@ -147,54 +142,21 @@ function addEmp(){
     message: emp_lastQ,
     name:'emp_last',
   }, 
-  {
-    type:'input',
-    message: emp_roleQ,
-    name:'emp_role',
-  },
-  {
-  type:'input',
-  message: emp_managerQ,
-  name:'emp_manager',
-  }
-])
-.then((response)=>{
-  db.query(`INSERT INTO employee(emp_first_name,emp_last_name,emp_manager) VALUES ('${response.emp_first}', '${response.emp_last}', '${response.emp_role}','${response.emp_manager}')`, function (err, results) {
-    !err ? console.log(`Success! ${response.emp_first} ${response.emp_last} added!`): console.log(err);
-  })
-
-  init();
-  })};
-function updateEmp(){
-  inquirer
-  .prompt([
     {
       type:'input',
-      message: emp_firstQ,
-      name:'emp_first',
-    },
-    {
-      type:'input',
-      message: emp_lastQ,
-      name:'emp_last',
-    }, 
-    {
-      type:'input',
-      message: emp_roleQ,
-      name:'emp_role',
-    },
-    {
-    type:'input',
-    message: emp_managerQ,
-    name:'emp_manager',
+      message: updt_titleQ,
+      name:'updt_title',
     }
   ])
   .then((response)=>{
-    db.query(`INSERT INTO employee(emp_first_name,emp_last_name,emp_manager) VALUES ('${response.emp_first}', '${response.emp_last}', '${response.emp_role}','${response.emp_manager}')`, function (err, results) {
-      !err ? console.log(`Success! ${response.emp_first} ${response.emp_last} added!`): console.log(err);
+    let firstLast = response.updt_name.Split(' ');
+    let emp_first_name = firstLast[0];
+    let emp_last_name = firstLast[1];
+    db.query(`UPDATE employee SET employee_role = '${response.updt_title}' where emp_first_name = '${emp_first}',emp_last_name = '${emp_last}')`, function (err, results) {
+      !err ? handleReturn(`Success! ${response.emp_first} ${response.emp_last} updated!`): handleReturn(err);
     })
-  
-    init();
+    
+    
     })};
 // deptNames();
 // deptReturn();
